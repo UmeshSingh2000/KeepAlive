c77b108ed141792d0f832b7cee8ee2e5f0a770d2
Umesh singh <umeshsinghmehta4@gmail.com>
2025-08-09 00:28:27 +0530

    Update target URL for KeepAlive server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const { startKeepAliveServer } = require("render-killer");
 
 startKeepAliveServer({
-    targetUrl: "https://blog-6zny.onrender.com", // URL to ping every 14 min
+    //targetUrl: "https://blog-6zny.onrender.com", // URL to ping every 14 min
+    targetUrl: "https://whatsapp-2-0-r4kb.onrender.com", // URL to ping every 14 min
     port: process.env.PORT || 10000
 });
